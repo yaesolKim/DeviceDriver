aa31575c785984a82ac7373ef7b721da859e2aef
yaesolKim <kimyaesol@gmail.com>
2025-07-24 15:02:15 +0900

    copy source codes

---
+++ Project8/device_driver.h
@@ -0,0 +1,14 @@
+#pragma once
+#pragma once
+#include "flash_memory_device.h"
+
+class DeviceDriver
+{
+public:
+    DeviceDriver(FlashMemoryDevice* hardware);
+    int read(long address);
+    void write(long address, int data);
+
+protected:
+    FlashMemoryDevice* m_hardware;
+};
