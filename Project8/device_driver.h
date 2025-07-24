7f411b88b20d65790a9fa2b4e737693a11ce0be7
yaesolKim <kimyaesol@gmail.com>
2025-07-24 16:59:50 +0900

    application

--- Project8/device_driver.h
+++ Project8/device_driver.h
@@ -11,4 +11,28 @@ public:
 
 protected:
     FlashMemoryDevice* m_hardware;
+
+private:
+    void writePreconditionCheck(long address);
+    void readPostConditionCheck(long address, int read);
+};
+
+class Application {
+public:
+    Application(DeviceDriver* dd) : dd{ dd } {}
+
+    void readAndprint(long startAddr, long endAddr) {
+        for (long addr = startAddr; addr <= endAddr; addr++) {
+            dd->read(addr);
+        }
+    }
+
+    void writeAll(unsigned char value) {
+        for (long addr = 0x00; addr <= 0x04; addr++) {
+            dd->write(addr, value);
+        }
+    }
+
+private:
+    DeviceDriver* dd;
 };
--- Project8/main.cpp
+++ Project8/main.cpp
@@ -5,66 +5,87 @@ using namespace testing;
 
 class FlashMock : public FlashMemoryDevice {
 public:
-	MOCK_METHOD(unsigned char, read, (long address), (override) );
-	MOCK_METHOD(void, write, (long address, unsigned char data), (override) );
+    MOCK_METHOD(unsigned char, read, (long address), (override));
+    MOCK_METHOD(void, write, (long address, unsigned char data), (override));
 };
 
+TEST(DeviceDriverRead, FiveRead) {
+    FlashMock mock;
 
-TEST(DeviceDriver, ReadFiveTimesSameReturn) {
-	FlashMock mock;
-	EXPECT_CALL(mock, read((long)0xB))
-		.Times(5)
-		.WillRepeatedly(Return(0xB));
+    EXPECT_CALL(mock, read((long)0xB))
+        .Times(5);
 
-	DeviceDriver driver{ &mock };
-	int data = driver.read((long)0xB);
-	EXPECT_EQ(data, 0xB);
+    DeviceDriver driver{ &mock };
+    int data = driver.read((long)0xB);
+}
 
-	//EXPECT_THROW({ unsigned char data = driver.read(0xFF); }, ReadFailException);
+TEST(DeviceDriverRead, ReadAndException) {
+    FlashMock mock;
 
+    EXPECT_CALL(mock, read((long)0xB))
+        .WillOnce(Return((unsigned char)0xA))
+        .WillOnce(Return((unsigned char)0xA))
+        .WillOnce(Return((unsigned char)0xA))
+        .WillOnce(Return((unsigned char)0xA))
+        .WillOnce(Return((unsigned char)0xB));
+
+    DeviceDriver driver{ &mock };
+    EXPECT_THROW(driver.read((long)0xB), std::exception);
+}
+
+TEST(DeviceDriverWrite, ReadBeforeWrite) {
+    NiceMock<FlashMock> mock;
+
+    EXPECT_CALL(mock, read((long)0xB))
+        .Times(1)
+        .WillRepeatedly(Return((unsigned char)0xFF));
+
+    DeviceDriver driver{ &mock };
+    driver.write((long)0xB, 7);
+}
+
+TEST(DeviceDriverWrite, WriteException) {
+    NiceMock<FlashMock> mock;
+
+    EXPECT_CALL(mock, read((long)0xB))
+        .WillRepeatedly(Return((unsigned char)0xFE));
+
+    DeviceDriver driver{ &mock };
+    EXPECT_THROW(driver.write((long)0xB, 7), std::exception);
 }
 
 
-TEST(DeviceDriver, ReadFiveTimesException) {
-	FlashMock mock;
+TEST(Application, ReadAndPrint) {
+    NiceMock<FlashMock> mock;
+    DeviceDriver dd{ &mock };
+    Application app{ &dd };
 
-	EXPECT_CALL(mock, read((long)0xB))
-		.Times(5)
-		.WillOnce(Return(0xA))
-		.WillOnce(Return(0xA))
-		.WillOnce(Return(0xA))
-		.WillOnce(Return(0xA))
-		.WillOnce(Return(0xB));
+    EXPECT_CALL(mock, read)
+        .Times(25);
 
-	DeviceDriver driver{ &mock };
-	EXPECT_THROW(driver.read((long)0xB), std::exception);
+    //콘솔출력 될것
+    app.readAndprint(0x00, 0x04);
+
+    //1. 안한다. 
+    //    내부적으로 랩퍼 클래스 반환값 Proxy 출력
+    //2. 콘솔 버퍼 옮겼다가 다시 원복 
+         //Trivia KATA
+    //3. 행동검증
 }
 
-TEST(DeviceDriverWrite, ReadBeforeWriteException) {
-	NiceMock<FlashMock> mock;
+TEST(Application, WriteAll) {
+    NiceMock<FlashMock> mock;
+    DeviceDriver dd{ &mock };
+    Application app{ &dd };
 
-	EXPECT_CALL(mock, read((long)0xB))
-		.Times(1)
-		.WillOnce(Return(0xB));
+    EXPECT_CALL(mock, read)
+        .Times(5)
+        .WillRepeatedly(Return((unsigned char)0xFF));
 
-	DeviceDriver driver{ &mock };
-	EXPECT_THROW(driver.write((long)0xB, 7), std::exception);
+    app.writeAll(0x77);
 }
-
-
-TEST(DeviceDriverWrite, CheckWrite) {
-	NiceMock<FlashMock> mock;
-	EXPECT_CALL(mock, read((long)0xB))
-		.WillRepeatedly(Return(0xFF));
-
-	DeviceDriver driver{ &mock };
-	driver.write((long)0xB, 7);
-
-}
-
-
 
 int main() {
-	::testing::InitGoogleMock();
-	return RUN_ALL_TESTS();
+    ::testing::InitGoogleMock();
+    return RUN_ALL_TESTS();
 }
